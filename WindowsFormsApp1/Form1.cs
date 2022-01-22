4fccfcbabf9129ef9e596283d827a2e0277e5b61
admin <admin@DESKTOP-OBCTT6C>
2022-01-22 17:56:41 +0530

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Stopwatch watch { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            watch = Stopwatch.StartNew();
+            port.Open();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            writeToPort(new Point(e.X, e.Y));
+        }
+        public void writeToPort(Point coordinates)
+        {
+            if (watch.ElapsedMilliseconds > 15)
+            {
+                watch = Stopwatch.StartNew();
+
+                port.Write(String.Format("X{0}Y{1}",
+                (180 - coordinates.X / (Size.Width / 180)),
+                (coordinates.Y / (Size.Height / 180))));
+            }            
+
+
+        }
+    }
+
+}
